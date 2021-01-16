5337e749b0c2476163899c0bb3a1f473fd53766f
MMK <retko85@gmail.com>
2021-01-16 17:52:23 +0100

    Update main.py
    menu

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ pygame.init()
 w = 600
 sidebar = 200
 grid_length = 12
+grid_from_settings = 12 # 8 / 12 / 20
+grid_length = grid_from_settings
 num_of_bombs = 34
 flags = []
 num_of_flags = 0
@@ -20,6 +22,8 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((w + sidebar, w))
 pygame.display.set_caption("minesweeper")
 pygame.display.set_icon(pygame.image.load('bombicon.png'))
+clicked = False #||
+counter = 0 #||
 
 # basic parameters
 BLACK = (0, 0, 0)
@@ -85,6 +89,8 @@ def draw_grid_lines():
         line_start = round((w / grid_length) * (line + 1))
         pygame.draw.line(screen, BLACK, (line_start, 0), (line_start, w), 6)
         pygame.draw.line(screen, BLACK, (0, line_start), (w, line_start), 6)
+
+    # 4 Cary okolo okna
     pygame.draw.line(screen, BLACK, (0, 0), (0, w), 6)
     pygame.draw.line(screen, BLACK, (0, 0), (w, 0), 6)
     pygame.draw.line(screen, BLACK, (w, 0), (w, w), 6)
@@ -118,6 +124,7 @@ def draw_side_text():
     draw_text = text_font.render(text, True, BLACK)
     screen.blit(draw_text, (w + 30, 560))
     text = "Basil Eagle 27/08/20"
+    text = "Team 03"
     draw_text = text_font.render(text, True, BLACK)
     screen.blit(draw_text, (w + 30, 580))
     if num_of_flags > 0 and playing:
@@ -251,7 +258,7 @@ def uncover_squares(x, y):
 
 def reset():
     global start_time, playing, bombs_loaded, num_of_flags, spaces_remaining, win
-    start_time = 0
+    start_time = 0 
     playing = True
     bombs_loaded = False
     corner_squares.clear()
@@ -301,8 +308,211 @@ while running:
                         spaces_remaining = update_grid()
                         if spaces_remaining < 1:
                             win = True
+#||
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+
+
+
+class button():
+		
+	#colours for button and text
+	button_col = (255, 0, 0)
+	hover_col = (75, 225, 255)
+	click_col = (50, 150, 255)
+	text_col = BLACK
+	button_width = 180
+	button_height = 70
+
+	def __init__(self, x, y, text):
+		self.x = x
+		self.y = y
+		self.text = text
+
+	def draw_button(self):
+
+		global clicked
+		action = False
+
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#create pygame Rect object for the button
+		button_rect = pygame.Rect(self.x, self.y, self.button_width, self.button_height)
+		
+		#check mouseover and clicked conditions
+		if button_rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1:
+				clicked = True
+				pygame.draw.rect(screen, self.click_col, button_rect)
+			elif pygame.mouse.get_pressed()[0] == 0 and clicked == True:
+				clicked = False
+				action = True
+			else:
+				pygame.draw.rect(screen, self.hover_col, button_rect)
+		else:
+			pygame.draw.rect(screen, self.button_col, button_rect)
+		
+		#add shading to button
+		pygame.draw.line(screen, WHITE, (self.x, self.y), (self.x + self.button_width, self.y), 2)
+		pygame.draw.line(screen, WHITE, (self.x, self.y), (self.x, self.y + self.button_height), 2)
+		pygame.draw.line(screen, BLACK, (self.x, self.y + self.button_height), (self.x + self.button_width, self.y + self.button_height), 2)
+		pygame.draw.line(screen, BLACK, (self.x + self.button_width, self.y), (self.x + self.button_width, self.y + self.button_height), 2)
+
+		#add text to button
+		text_img = text_font.render(self.text, True, self.text_col)
+		text_len = text_img.get_width()
+		screen.blit(text_img, (self.x + int(self.button_width / 2) - int(text_len / 2), self.y + 25))
+		return action
+
+
+
+
+
+
+# ||
+def game():
+    global running, playing, bombs_loaded, capture
+
+    # game loop
+    running = True
+    playing = True
+    bombs_loaded = False
+
+    while running:
+        screen.fill(GREY)
+
+        draw_grid_lines()
+        draw_side_text()
+        draw_graphics()
+        
+        mouseX, mouseY = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit() # Needed for cross button to quit correctly
+                exit() 
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    reset()
+            if event.type == pygame.MOUSEBUTTONDOWN and playing:
+                if mouseX < w and pygame.mouse.get_pressed() == (1, 0, 0):
+                    gridX, gridY = find_grid_coords(mouseX, mouseY)
+                    gridXY = gridX, gridY
+                    if not bombs_loaded:
+                        load_bombs(gridXY)
+                        bombs_loaded = True
+                        start_time = pygame.time.get_ticks()
+                    if gridXY not in discovered_squares:
+                        if gridXY in bomb_squares:
+                            win = False
                             playing = False
                             capture = ticking
+                        else:
+                            discovered_squares.append(gridXY)
+                            uncover_squares(gridX, gridY)
+                            spaces_remaining = update_grid()
+                            if spaces_remaining < 1:
+                                win = True
+                                playing = False
+                                capture = ticking
+
+                elif mouseX < w and pygame.mouse.get_pressed() == (0, 0, 1):
+                    gridX, gridY = find_grid_coords(mouseX, mouseY)
+                    gridXY = gridX, gridY
+                    if gridXY not in discovered_squares:
+                        get_flag(gridXY)
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+
+
+#||
+def main_menu():
+    global w
+
+    # Creating Buttons
+    play_button = button(w/2, w/2-100, 'Play')
+    options_button = button(w/2, w/2, 'Options')
+    quit_button = button(w/2, w/2+100, 'Quit')
+
+
+    while True:
+        
+        screen.fill((0,0,0))
+        draw_text('Main Menu', text_font, (255, 255, 255), screen, 20, 20)
+
+
+        if play_button.draw_button():
+            game()
+        if options_button.draw_button():
+            options()
+        if quit_button.draw_button():
+            pygame.quit()
+            exit()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    exit()
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+
+def options():
+    global grid_from_settings
+
+    # Creating Buttons
+    small = button(w/2, w/2-100, 'Small')
+    medium = button(w/2, w/2, 'Medium')
+    high = button(w/2, w/2+100, 'High')
+
+
+    running = True
+    while running:
+
+        screen.fill((0,0,0))
+
+        # Text Options
+        draw_text('Options', text_font, (255, 255, 255), screen, 20, 20)
+
+        # Actions when button is clicked
+        if small.draw_button():
+            grid_from_settings = 8
+            game()
+        if medium.draw_button():
+            grid_from_settings = 12
+            game()
+        if high.draw_button():
+            grid_from_settings = 20
+            game()
+
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit() # Needed for cross button to quit correctly
+                exit() 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+        
+        pygame.display.update()
+        clock.tick(60)
+
 
             elif mouseX < w and pygame.mouse.get_pressed() == (0, 0, 1):
                 gridX, gridY = find_grid_coords(mouseX, mouseY)
@@ -313,3 +523,5 @@ while running:
     pygame.display.update()
     clock.tick(60)
 
+if __name__ == '__main__':
+    main_menu()
